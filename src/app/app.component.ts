c92d34267b4655d7ff459244d98fe199874f65f4
dis0400 <alexiad.marinm@gmail.com>
2024-09-20 21:46:23 -0400

    [20-09-2024] print just pares

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,28 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title:number = 10;
-  
-  constructor(){
+  title: number = 10;
 
+  constructor() {
+    console.log('subtract ', this.subtract(8, 4))
+
+  }
+
+  public sum(num1: number, num2: number): number {
+    return num1 + num2
+  }
+
+  public subtract(num1: number, num2: number): number {
+    return num1 - num2
+  }
+
+  public getArray() {
+    const persons: number[] = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    const evenNumbers = persons.filter(person => person % 2 === 0);
+
+    for (let i = 0; i < evenNumbers.length; i++) {
+      console.log('even person =', evenNumbers[i]);
+    }
   }
 }
