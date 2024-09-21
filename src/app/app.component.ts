2c821a6bb0e3639fea7b1db71c7c3035f23d51ed
dis0400 <alexiad.marinm@gmail.com>
2024-09-20 22:24:48 -0400

    [20-09-2024] interface and operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface Person{
+  name: string,
+  lastName: string,
+  age?: number
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,10 +16,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title: number = 10;
+  animals:string[] = ['a','b','c','d','e','f','g'];
+  person: Person = {
+    name: 'Alexia',
+    lastName: 'Marin',
+    age: 22
+  }
 
   constructor() {
     console.log('subtract ', this.subtract(8, 4))
 
+    console.log('MAP:', this.animals.map((animal)=> (animal + ' new')))
+    console.log('FOREACH:', this.animals.forEach((animal)=> (animal + ' new')))
+    console.log('FIND:', this.animals.find((animal)=> animal==='z'))
+    console.log('FILTER:', this.animals.filter((animal)=> animal==='y'))
+    console.log('INDEXOF:', this.animals.indexOf('c'))
   }
 
   public sum(num1: number, num2: number): number {
