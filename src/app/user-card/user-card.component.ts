8299b47211eae87f933d9ae25f7e6acfb21c708e
Alexia Marin <alexiad.marinm@gmail.com>
2024-10-01 20:32:46 -0400

    [30-09-2024] ngOnChanges

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from "@angular/core";
 
 
 @Component({
@@ -9,21 +9,28 @@ import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from "@angu
   templateUrl: "./user-card.component.html",
   styleUrl: "./user-card.component.scss"
 })
-export class UserCardComponent {
+export class UserCardComponent implements OnInit, OnDestroy, OnChanges{
 
   @Input() name:string= ""
   @Input() email:string= ""
 
   @Output() sendData = new EventEmitter()
+
+  password: string = ""
   constructor() {
     console.log("user card constructor");
   }
 
   ngOnInit(): void {
     console.log("user card on init");
+    // this.password = this.name + ' ' +  this.email + ' PASSWORD'
   }
   ngOnDestroy(): void {
     console.log('user card Destroy')  
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('CHANGES:', changes)
+    this.password = changes['name'].currentValue + ' ' + changes['email'].currentValue + ' PASSWORD'
   }
 
   public onSendData() {
