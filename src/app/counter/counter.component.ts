caee34cc2b0040b14cfbb40c9b4450a953e3b590
Alexia Marin <alexiad.marinm@gmail.com>
2024-10-06 03:32:54 -0400

    [03-10-2024] phase write and read, attributes directives ngClass

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -12,28 +12,22 @@ export class CounterComponent {
 
   constructor() {
     afterRender({
-      earlyRead: () => {
-        console.log('INTO earlyRead')
-        const currentAppColor = this.appBackground
-        return 'From earlyRead: ' + currentAppColor
+       write:() => {
+          console.log('INTO write')
+          document.body.style.backgroundColor = this.appBackground
+          const currentColor = this.appBackground
+          if(currentColor === 'red') {
+            this.appBackground = 'blue'
+          } else {
+            this.appBackground = 'red'
+          }
+          return 'From mixedReadWrite: ' + this.appBackground
       },
-      mixedReadWrite: (props) => {
-        console.log('INTO mixedReadWrite ', props)
-        if(props.indexOf('red') > -1) {
-          this.appBackground = 'green'
-        } else {
-          this.appBackground = 'red'
-        }
-        return 'From mixedReadWrite: ' + this.appBackground
-      },
-      write:(props) => {
-        console.log('INTO write ', props)
-        document.body.style.backgroundColor = this.appBackground
-        return 'FROM write: ' + this.appBackground
-      },
+      
       read:(props) => {
         console.log('INTO read ', props)
         const newBackground = this.appBackground
+        console.log('FROM read: ', newBackground)
       },
     })
     afterNextRender(() => {
