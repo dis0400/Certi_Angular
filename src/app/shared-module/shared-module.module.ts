ef43c249d3dc5f2c0fba71d0c2fd15fa5ce6bec1
Alexia Marin <alexiad.marinm@gmail.com>
2024-10-08 23:36:59 -0400

    [07-10-2024] shared-nodule, alert, card, ng-styles, navbar

---
+++ src/app/shared-module/shared-module.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AlertComponent } from '../alert/alert.component';
+import { CardComponent } from '../card/card.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+@NgModule({
+  declarations: [
+    AlertComponent, 
+    CardComponent,
+    NavbarComponent], //Van los componetes, directivas, pipes.
+  imports: [
+    CommonModule
+  ] //Se manejan los mudulos
+})
+export class SharedNoduleModule { 
+}
