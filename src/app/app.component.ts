d8235033100490a80758a9044e3d1292ae2db37d
Alexia Marin <alexiad.marinm@gmail.com>
2024-10-08 23:52:18 -0400

    [08-10-2024] observables first example, concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,25 +4,34 @@ import { UserCardComponent } from './user-card/user-card.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { CommonModule } from '@angular/common';
 import { CounterComponent } from './counter/counter.component';
+import { filter, from, map, tap } from "rxjs";
 
 interface IPerson{
   name: string,
   lastName: string,
-  gender: string,
   age?: number
 }
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, UserCardComponent, CalculatorComponent,CommonModule, CounterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    RouterOutlet,
+    UserCardComponent,
+    CalculatorComponent,
+    CommonModule,
+    CounterComponent,
+  ],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
 
-  users = [{ name: 'abc', 'email': 'abc@gmail.com' }, { name: 'dfg', 'email': 'dfg@gmail.com' }]
-  selectedUser:any = this.users[0];
+  users = [
+    { name: "abc", email: "abc@gmail.com" },
+    { name: "dfg", email: "dfg@gmail.com" },
+  ];
+  selectedUser: any = this.users[0];
   
   userCardCreated: boolean = true 
   result: number = 0;
@@ -32,73 +41,49 @@ export class AppComponent {
   person: IPerson = {
     name: "Alexia",
     lastName: "Marin",
-    gender: "female", 
     age: 22
-  }
-
-  persons: IPerson[] = [
-    { name: 'Alexia', lastName: 'Marin', gender: 'female', age: 19 },
-    { name: 'Juan', lastName: 'Perez', gender: 'male', age: 15 },
-    { name: 'Maria', lastName: 'Lopez', gender: 'female', age: 20 },
-  ];
-
-  get totalFemales(): number {
-    return this.persons.filter(person => person.gender === 'female').length;
-  }
-
-  get totalMales(): number {
-    return this.persons.filter(person => person.gender === 'male').length;
-  }
-
-  get totalWithDiscount(): number {
-    return this.persons.filter(person => person.age !== undefined && person.age > 18).length;
-  }
-
-  showDiscountedOnly: boolean = false;
-
-  toggleDiscountFilter(): void {
-    this.showDiscountedOnly = !this.showDiscountedOnly;
-  }
-
-  get filteredPersons(): IPerson[] {
-    if (this.showDiscountedOnly) {
-      return this.persons.filter(person => person.age !== undefined && person.age > 18);
-    }
-    return this.persons;
   }
 
   students:number[] = [1,2,3,4,5,6]
   parents:number[] = [7,8,9,10]
 
-  var1 = 0
-  var2 = null
-  var3 = 'hola'
+  var1 = 0;
+  var2 = null;
+  var3 = "hola";
+  youtube = from([1, 2, 3, 4, 5, 6]);
 
   constructor() {
-    const {name, age} = this.person
-    //console.log('desestructuration  ', name, age)
-
-    let both =[...this.students, ...this.parents]
-    // console.log('spreed operator: ', both)
-    // console.log('REST operator: ', this.sum(2,4,6))
-
-    // console.log('Nullish coalesing: ', this.var2 ?? this.var3)
-    // console.log('OR: ', this.var2 || this.var1)
-
-    // console.log('subtract ', this.subtract(8, 4))
-
-    // console.log('MAP:', this.animals.map((animal)=> (animal + ' new')))
-    // console.log('FOREACH:', this.animals.forEach((animal)=> (animal + ' new')))
-    // console.log('FIND:', this.animals.find((animal)=> animal==='z'))
-    // console.log('FILTER:', this.animals.filter((animal)=> animal==='y'))
-    // console.log('INDEXOF:', this.animals.indexOf('c'))
+    const { name, age } = this.person;
+    let both = [...this.students, ...this.parents];
+    this.youtube.subscribe((res) => {
+      console.log("SUSCRIBER 1: ", res);
+    });
   }
 
   public sum(...persons:number[]){
     //return persons[0] + persons[1]
     return persons.reduce(
-      (accumulator, currentValue) => accumulator + currentValue, 10
-    )
+      (acumulador, valorActual) => acumulador + valorActual,
+      10
+    );
+  }
+  addVideo() {
+    this.youtube
+      .pipe(
+        map((res: number) => {
+          //console.log("MAP OPERATOER RXJS: ", res);
+          if (res % 2 === 0) {
+            return res;
+          } else {
+            return null
+          }
+        }),
+        tap((res)  => {console.log('VALUE: ', res)}),
+        filter((res: number | null) => res !== null),
+      )
+      .subscribe((res) => {
+        console.log("SUSCRIBER 2: ", res);
+      });
   }
 
   public sum2(num1: number, num2: number): number {
