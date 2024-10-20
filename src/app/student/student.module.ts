010758865c7875654307ff5684fe815e39017602
Alexia Marin <alexiad.marinm@gmail.com>
2024-10-20 01:51:56 -0400

    [17-10-2024] navigation from html

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentRoutingModule } from './student-routing.module';
 import { ScoreComponent } from './score/score.component';
+import { ClassmateComponent } from './classmate/classmate.component';
+import { AverageComponent } from './average/average.component';
+
 @NgModule({
   declarations: [
     ScoreComponent
   ],
   imports: [
     CommonModule,
-    StudentRoutingModule
+    StudentRoutingModule,
+    ScoreComponent,
+    ClassmateComponent,
+    AverageComponent
   ]
 })
 export class StudentModule { }
