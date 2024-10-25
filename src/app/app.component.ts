9619d757c611d6506805d15e460db030c459232e
Alexia Marin <alexiad.marinm@gmail.com>
2024-10-25 07:50:26 -0400

    [23-10-2024] validations in template driven form and reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -123,6 +123,9 @@ export class AppComponent {
     }) */
 
   }
+  print(){
+    console.log('FORM NAME: ', this.studentForm.get('name'))
+  }
   onSendData() {
     console.log('FORM GROUP: ', this.studentForm)
   }
