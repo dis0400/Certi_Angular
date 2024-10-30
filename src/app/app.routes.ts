256f513bb1578babb495c58a85077269f3a38e05
Alexia Marin <alexiad.marinm@gmail.com>
2024-10-30 08:35:23 -0400

    [25-010-2024] guard CanActivate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,29 @@
 import { Routes } from '@angular/router';
 import { UserCardComponent } from './user-card/user-card.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-        {
-            path: 'card/:studentId',
+    {
+        path: 'card/:studentId',
         component: UserCardComponent,
         title: 'user card test tilte'
-        },
-        {
-            path: 'cal',
-            component: CalculatorComponent
-        },
-        {
-            path: 'counter-nav',
-            loadComponent: () => import('./counter/counter.component').then(c => {
-                // we can put more logic here
-                return c.CounterComponent
-            })
-        },
-        {
-            path: 'student',
-            loadChildren: () => import('./student/student.module').then(m => m.StudentModule)
-        },
+    },
+    {
+        path: 'cal',
+        component: CalculatorComponent
+    },
+    {
+        path: 'counter-nav',
+        loadComponent: () => import('./counter/counter.component').then(c => {
+            // we can put more logic here
+            return c.CounterComponent
+        })
+    },
+    {
+        path: 'student',
+        title: 'Student',
+        canActivate: [AuthGuard],
+        loadChildren: () => import('./student/student.module').then(m => m.StudentModule)
+    }
 ];
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,8 +3,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private token:string = ''
-  private user: string = ''
+  private token!:string
+  private user!: string
+  
   constructor() { }
   public login(){
     this.token = '123ABC'
