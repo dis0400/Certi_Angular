10644fcf58669a56d2ce2dd5b069856ac7546010
Alexia Marin <alexiad.marinm@gmail.com>
2024-11-04 20:18:31 -0400

    [04-11-2024] Auth interceptor

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,20 +3,28 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private token!:string
+  private token!:string | null
   private user!: string
   
   constructor() { }
   public login(){
     this.token = '123ABC'
     this.user = 'test@gmail.com'
+    localStorage.setItem('token', this.token)
     return this.token
   }
   public logout(){
     this.token = ''
     this.user = ''
+    localStorage.removeItem('token')
   }
   public getUser(){
     return this.user
+  }
+
+  public getToken(){
+    const token = localStorage.getItem('token')
+    this.token = token ?? ''
+    return this.token
   }
 }
